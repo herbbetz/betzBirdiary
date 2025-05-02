0d8ef420b855ad82e98bbc23da02f49e9f53dc8c
Herbert BETZ <herbertbetz@csi.com>
2025-05-02 14:33:04 +0200

    Add files via upload

---
+++ station2/mybirds/species.js
@@ -0,0 +1,11 @@
+records = new Array(
+"gruenfink28-4-2025|Grünfink",
+"jungfink2025-04-18|Jungfink",
+"kernbeisser|Kernbeisser",
+"kleiber2024-07-25|Kleiber",
+"meise2025-04-20|Kohlmeise",
+"rotkehl2024-11-07|Rotkehlchen",
+"rotkehljung|Rotkehlchen jung",
+"rotschwanz-vorn24-4-2025|Rotschwanz",
+"spatz2024-10-28|Spatz"
+);
