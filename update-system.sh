204baaeca8d42f18245afa8daa2dcec368b4d04f
herbbetz <herbertbetz@csi.com>
2025-11-25 09:06:32 +0100

    herbUpdate 2025-11-25-09-06

--- update-system.sh
+++ update-system.sh
@@ -3,6 +3,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
+echo "Update complete! Press enter to exit."
+read -r # for 'screen update-system.sh', Ctrl-A,D to be left open to reattach using 'screen -r', when long update finishes that was started in ssh session
+# alternative: './your_script.sh > update.log 2>&1 &', then 'disown' to detach from ssh session and later 'cat update.log' or 'tail -f update.log' to watch progress
+
 # /home/pi/birdvenv/bin/pip install --upgrade pip
 
 # Export only venv packages by --local (not system)
