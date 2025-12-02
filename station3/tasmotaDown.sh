4959eccd6a80f338c892e1e0be865602d327824c
herbbetz <herbertbetz@csi.com>
2025-12-02 23:25:07 +0100

    herbUpdate 2025-12-02-23-25

--- station3/tasmotaDown.sh
+++ station3/tasmotaDown.sh
@@ -12,12 +12,12 @@ fi
 
 bash "$APPDIR/mdroid.sh" "$msg"
 # https://tasmota.github.io/docs/Commands/
-# %20 = ' ', %3B = ';', Backlog = 'command sequence with ; separator', Delay 1200 = '1200 * 0.1secs'
+# %20 = ' ', %3B = ';', Backlog = 'command sequence with ; separator', Delay 1200 = '1200 * 0.1secs', Power means Power1 = relay 1
 # /usr/bin/curl "http://192.168.178.50/cm?cmnd=Backlog%20Delay%201200%3BPower%20off"
 CNT=0
 while [[ $CNT -lt 3 ]]; do
     sleep 5
-    status_code=$(/usr/bin/curl -s -o /dev/null -w "%{http_code}" "http://$tasmota_ip/cm?cmnd=Backlog%20Delay%201200%3BPower%20off")
+    status_code=$(/usr/bin/curl -s -o /dev/null -w "%{http_code}" "http://$tasmota_ip/cm?cmnd=Backlog%20Delay%201200%3BPower1%20off")
     # echo $CNT: $status_code
     if [[ $status_code == 200 ]]; then
         break
